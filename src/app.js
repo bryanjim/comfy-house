a3d21efaaae8aace1c25f920a7833355027283bf
Bryan <bryan@roguedevs.io>
2019-05-28 16:23:06 -0700

    Added dynamic display of images

--- src/app.js
+++ src/app.js
@@ -19,7 +19,14 @@ class Products{
         try {
             let result = await fetch("products.json");
             let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item => {
+                const{title, price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image}
+            })
+            return products
         } catch (error) {
             console.log(error);
         }
@@ -29,7 +36,25 @@ class Products{
 
 // Display products
 class UI {
-
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to bag
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+            `
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 // local storage
@@ -41,5 +66,5 @@ document.addEventListener("DOMContentLoaded", () =>{
     const ui = new UI();
     const products = new Products();
     // get all products
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => ui.displayProducts(products));
 })
