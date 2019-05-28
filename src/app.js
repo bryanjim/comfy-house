82e4e45cb92d4d32b9c180a69683f33bebadafab
Bryan <bryan@roguedevs.io>
2019-05-28 16:11:01 -0700

    Fixed bug in index.html and added vars in app.js

--- src/app.js
+++ src/app.js
@@ -9,3 +9,37 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
+
+// Main cart
+let cart = []
+
+// Retrieving the products
+class Products{
+    async getProducts(){
+        try {
+            let result = await fetch("products.json");
+            let data = await result.json();
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+        
+    }
+}
+
+// Display products
+class UI {
+
+}
+
+// local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const ui = new UI();
+    const products = new Products();
+    // get all products
+    products.getProducts().then(data => console.log(data));
+})
